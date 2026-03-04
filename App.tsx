03c0d33a523622a3a71c02962805ed1008e85424
Rythe25 <kyseanghor232@gmail.com>
2026-03-04 21:12:53 +0700

    Fixed white flash

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
 import AuthStackNavigation from "./src/navigation/Auth/AuthStackNaivagtion";
+import * as SystemUI from 'expo-system-ui';
 
 
 export default function App() {
+  SystemUI.setBackgroundColorAsync("#171725");
   return (
     <AuthStackNavigation/>
   );
